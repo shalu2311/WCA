de70070f1ee711a2217a4d0c215c756d2c5a06c9
shalu2311 <42956133+shalu2311@users.noreply.github.com>
2025-03-25 12:30:23 +0530

    Update Book.py
    added print statement

--- Book.py
+++ Book.py
@@ -3,12 +3,4 @@ class Book:
         self.id = id
         self.name = name
         self.quantity = int(quantity)
-
-
-
-print("This is a sample comment!")
-class Pen:
-    def __init__(self, id, name, quantity):
-        self.id = id
-        self.name = name
-        self.quantity = int(quantity)
+        print(quantity)
