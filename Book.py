0acace268d4c0fb201e5fbc17f562598b1af3610
shalu2311 <42956133+shalu2311@users.noreply.github.com>
2025-03-25 12:26:38 +0530

    Update Book.py

--- Book.py
+++ Book.py
@@ -7,3 +7,8 @@ class Book:
 
 
 print("This is a sample comment!")
+class Pen:
+    def __init__(self, id, name, quantity):
+        self.id = id
+        self.name = name
+        self.quantity = int(quantity)
